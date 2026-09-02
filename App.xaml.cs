91741828fdb12fe4e696c02f4b77a09a3be472ed
Rohit Kumar Mallick <rohitmallick85@gmail.com>
2026-09-02 09:04:34 +0530

    Add Settings task to BezelStickNotes Jump List

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Windows;
+using System.Windows.Shell;
 using StickyNotes.Storage;
 
 namespace StickyNotes;
@@ -13,6 +14,7 @@ public partial class App : Application
     {
         base.OnStartup(e);
         await NoteStore.InitializeAsync();
+        ConfigureJumpList();
 
         bool settingsOnly = e.Args.Any(arg =>
             string.Equals(
@@ -36,6 +38,41 @@ public partial class App : Application
         MainWindow = mainWindow;
         mainWindow.Show();
     }
+    private static void ConfigureJumpList()
+    {
+        try
+        {
+            var executablePath = Environment.ProcessPath;
+
+            if (string.IsNullOrWhiteSpace(executablePath))
+            {
+                return;
+            }
+
+            var jumpList = new JumpList
+            {
+                ShowRecentCategory = false,
+                ShowFrequentCategory = false
+            };
+
+            jumpList.JumpItems.Add(new JumpTask
+            {
+                Title = "Settings",
+                Description = "Open BezelStickNotes Settings",
+                ApplicationPath = executablePath,
+                Arguments = "--settings",
+                IconResourcePath = executablePath
+            });
+
+            JumpList.SetJumpList(Current, jumpList);
+            jumpList.Apply();
+        }
+        catch
+        {
+            // Jump Lists are a convenience only; app startup must never fail.
+        }
+    }
 }
+
 
 
